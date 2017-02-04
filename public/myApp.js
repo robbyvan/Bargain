5a3434f3c674b84c00f89397aabf88b9c7958bf8
robbyvan <fzpinus@gmail.com>
2017-02-04 05:13:23 -0800

    correct file name

--- public/myApp.js
+++ public/myApp.js
@@ -3,7 +3,7 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(function($routeProvider){
   $routeProvider.when('/', {
     controller: 'UserController',
-    templateUrl: 'views/login.html'
+    templateUrl: 'views/register.html'
   })
   .otherwise({
     redirectTo: '/'
