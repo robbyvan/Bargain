ca52b1dea772bfd2a205ef808e2a478af2cbe28e
robbyvan <fzpinus@gmail.com>
2017-02-12 02:17:34 -0800

    save

--- public/controllers/admins.js
+++ public/controllers/admins.js
@@ -11,10 +11,38 @@ app.controller('ItemController', ['$scope', '$http', '$routeParams', '$location'
       $scope.adminItems = response.data;
       console.log($scope.adminItems);
     });
+  } 
+
+  $scope.getAdminItemById = function(id){
+      $http.get('/api/admin_items/' + id).then(function(response){
+      $scope.currentItem = response.data;
+      console.log('data get at: ' + Date.now());
+      console.log($scope.currentItem);
+    });
   }
 
-  $scope.addAdminItem = function(){
-    
+  $scope.addToMall = function(word){
+    console.log(word);
+    // $http.post('api/items', $scope.currentItem).then(function(response){
+    //   console.log('added.');
+    //   if (callback !== undefined){
+    //     callback();
+    //   }
+    // });
   }
+
+  $scope.removeAdminItem = function(id){
+    console.log('id is: ' + id);
+    $http.delete('/api/admin_items/' + id).then(function(response){
+      $scope.adminItems = response.data;
+      console.log('removed.');
+    });
+  }
+
+  $scope.addThenRemove = function(id){
+    // $scope.removeAdminItem(id)
+    $scope.getAdminItemById(id).then($scope.addToMall('excuted or not?'));
+  }
+  
 
 }]);
